61277bf253bee834b8f91e6afc27e4e9e35fcf3b
khushalhiranii <khushal.hirani4@gmail.com>
2024-06-25 01:52:42 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
-import { UserDataProvider } from './context/UserDataContext.jsx'
-import { Inputbar } from './components/Inputbar.jsx'
-import { Otp } from './components/otp.jsx'
-import { OtpVerified } from './components/OtpVerfied.jsx'
-import App from './App.jsx'
-import PrivateRoute from './routes/PrivateRoutes.jsx'
+import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <UserDataProvider>
   <Router>
-    <Routes>
-      <Route path='/' element={<App/>}>
-        <Route index element={<Inputbar/>}/>
-        <Route path='/otp' element={<PrivateRoute component={Otp} />}/>
-        <Route path='/otpverified' element={<PrivateRoute component={OtpVerified}/>}/>
-      </Route>
-    </Routes>
-  </Router>
- </UserDataProvider>
+    <App/>
+ </Router>
 )
